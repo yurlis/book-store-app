86b995959651f9e6878f948a2c1e0b643b9083ea
yurlis <yuri.lisovski@gmail.com>
2024-03-23 09:39:13 +0200

    Correct recommendations. use content-path property. Make handwriting getters and setters.

--- src/main/java/com/bookstoreapp/model/Book.java
+++ src/main/java/com/bookstoreapp/model/Book.java
@@ -7,9 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "books")
 public class Book {
@@ -23,4 +21,63 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+
+    public Book() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
 }
