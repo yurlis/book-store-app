bdc82c6c1e098159f83da77c0052e353f97c034e
yurlis <yuri.lisovski@gmail.com>
2024-03-31 23:21:36 +0300

    Add CustomGlobalExceptionHandler class Add meaningful validation for existing DTO classes

--- src/main/java/com/bookstoreapp/exception/EntityNotFoundException.java
+++ src/main/java/com/bookstoreapp/exception/EntityNotFoundException.java
@@ -1,7 +1,13 @@
 package com.bookstoreapp.exception;
 
+import java.util.function.Supplier;
+
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
+    }
+
+    public static Supplier<EntityNotFoundException> supplier(String message) {
+        return () -> new EntityNotFoundException(message);
     }
 }
