64f563cb9345b0fceca344d38a9f133ab589e31e
yurlis <yuri.lisovski@gmail.com>
2024-04-01 20:48:17 +0300

    Add Pagination, Sorting, and Swagger to the existing controllers

--- src/main/java/com/bookstoreapp/service/BookService.java
+++ src/main/java/com/bookstoreapp/service/BookService.java
@@ -3,14 +3,14 @@ package com.bookstoreapp.service;
 import com.bookstoreapp.dto.BookDto;
 import com.bookstoreapp.dto.BookSearchParameters;
 import com.bookstoreapp.dto.CreateBookRequestDto;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
